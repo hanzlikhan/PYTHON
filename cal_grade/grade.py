7d99bb7b3ae764ad4d37660598624f4167ff2b27
hanzlikhan <khangormani79@gmail.com>
2024-03-21 22:36:32 +0500

    init

---
+++ cal_grade/grade.py
@@ -0,0 +1,11 @@
+marks = 76
+if(marks>=90):
+    print("Grade", "A+")
+elif(marks >=85 and marks <90):
+    print("Grade","A")
+elif(marks >=80 and marks < 85) :
+    print("Grade", "A-")
+elif(marks >=75 and marks < 80) :
+    print("Grade", "B")
+else:
+    print("Invalid Marks")
