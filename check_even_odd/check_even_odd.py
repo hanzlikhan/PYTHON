9552e68672a20ab568bf72488404df62b93aee0b
hanzlikhan <khangormani79@gmail.com>
2024-03-21 23:13:21 +0500

    init

---
+++ check_even_odd/check_even_odd.py
@@ -0,0 +1,5 @@
+num = int(input("Enter a number :"))
+if(num%2==0):
+    print("your entered number is even")
+else:
+     print("your entered number is odd")
---
+++ find_greatest/find_greatest.py
@@ -0,0 +1,9 @@
+a = int(input("a :"))
+b = int(input("b :"))
+c = int(input("c :"))
+if(a>b and b>c):
+    print(a,"is greater")
+elif(b>a):
+    print(b,"is greater")
+else:
+    print(c,"is greater")
