eae06dd3352c3570daebf658edb4cccaab4f5dde
hanzlikhan <khangormani79@gmail.com>
2024-03-22 05:11:51 +0500

    ini

---
+++ create_list/create_list.py
@@ -0,0 +1,9 @@
+a = str(input("enter first fvrt movie :"))
+b = str(input("enter 2nd fvrt movie :"))
+c = str(input("enter third fvrt movie :"))
+movies = []
+movies.append(a)
+movies.append(b)
+movies.append(c)
+movies.append(input("enter a movie :"))
+print(movies)
