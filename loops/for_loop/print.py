1d00a273746bd37b01dd9e09af948d8522549406
hanzlikhan <khangormani79@gmail.com>
2024-03-24 04:12:56 +0500

    init

--- loops/for_loop/print.py
+++ loops/for_loop/print.py
@@ -2,6 +2,7 @@ nums = (1,4,9,16,25,36,49,64,81)
 x = 49
 for el in nums:
     if(x==el):
-        print(el)
-
-    
+        print("found i the list")
+        break
+    else:
+        print("not found")
