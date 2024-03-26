04f89546ad3f09d67cdcf2786c36853d65059157
hanzlikhan <khangormani79@gmail.com>
2024-03-26 06:12:36 +0500

    init

---
+++ pract/pac.py
@@ -0,0 +1,7 @@
+for i in range(1,10,1):
+    num = int(input("enter  number :"))
+    if(num%2==0):
+        print("even number")
+    else:
+        print("odd number")
+print("this is end of program")
